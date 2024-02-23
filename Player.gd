664b1998c7f621af0f57976e3c8a366b7f1000be
WOCae <t-satoIZ@outlook.jp>
2024-02-23 21:20:08 +0900

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -17,3 +17,12 @@ func _process(delta):
 
 	position += velocity.normalized() * speed
 	
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print("Mouse Click/Unclick at: ", event.position)
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+
+   # Print the size of the viewport.
+	print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
