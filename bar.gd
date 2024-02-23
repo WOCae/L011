40ec00dde7968c49dd2fbdf796a7e1e8f6fa180b
WOCae <t-satoIZ@outlook.jp>
2024-02-23 11:48:32 +0900

    Update bar.gd

--- bar.gd
+++ bar.gd
@@ -1,3 +1,5 @@
+#別のシーンとして、必要に応じてplayerに付与する。
+
 extends CharacterBody2D
 #extends Area2D
 #var velocity = Vector2()
@@ -8,15 +10,17 @@ func start(x, y, speed):
 	#velocity.y = -speed
 
 func _ready():
-	$CollisionShape2D.position.x = 20
-	
+	$CollisionShape2D.position.x = 50
+	$CollisionShape2D.scale = Vector2(10, 1)
 
 var timecount = 0
 var maxcount = 10
 #var cnt = 0
 #@export var Shotcnt = 1.5 #射出間隔
 func _process(delta):
-
+	position.x = CommonPlayer.player_X
+	position.y = CommonPlayer.player_Y
+	
 	if Input.is_action_pressed(("ui_select")):
 		#cnt += delta
 		#if cnt > Shotcnt:
@@ -25,13 +29,14 @@ func _process(delta):
 		if timecount <= maxcount:
 			timecount = timecount + 1
 				
-			rotation = -10 *delta*timecount
+			rotation = -10 *delta*timecount + CommonPlayer.player_R
 
 		else:
 			#timecount = 0
-			rotation = 0
+			rotation = 0 + CommonPlayer.player_R
 		#for i in range(18):
 			#rotation = -3*i
 	if Input.is_action_just_released(("ui_select")):
-		rotation = 0
+		rotation = 0 + CommonPlayer.player_R
 		pass 
+
